df08acc5eaca353c95223d6e2558dbc4c3c565dc
Amadeus <amadeus@myself.com>
2020-09-13 11:35:34 -0500

    Initial Commit

---
+++ Broker_Recount.lua
@@ -0,0 +1,29 @@
+﻿local dataobj = LibStub:GetLibrary("LibDataBroker-1.1"):NewDataObject("Recount", {
+	type = "launcher",
+	icon = "Interface\\AddOns\\Broker_Recount\\Images\\icon",
+	label = "Recount",
+	text = "Recount",
+	name = "Recount",
+	OnClick = function(_, button)
+		if button=="LeftButton" then
+			if Recount.MainWindow:IsVisible() then
+				Recount.MainWindow:Hide()
+			else
+				Recount.MainWindow:Show()
+				Recount:RefreshMainWindow()
+			end
+		elseif button=="RightButton" then
+			if not Recount.ConfigWindow or not Recount.ConfigWindow:IsVisible() then
+				Recount:ShowConfig()
+			else
+				Recount.ConfigWindow:Hide()
+			end
+		end
+	end
+})
+
+function dataobj:OnTooltipShow()
+	self:AddLine("|cffffffff" .. "Recount|r")
+	self:AddLine("|cffffff00" .. "Left-Click|r to toggle the Recount window")
+	self:AddLine("|cffffff00" .. "Right-click|r to open the options menu")
+end
